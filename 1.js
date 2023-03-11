6b601958422b7e1243a25b70807413a97210f691
ReshmiAjay2022 <100823647+ReshmiAjay2022@users.noreply.github.com>
2023-03-11 11:06:26 +0530

    Reshmi Ajay

---
+++ 1.js
@@ -0,0 +1,5 @@
+const rootvalue=document.getElementById('root');
+const newvalue=document.createElement('div');
+newvalue.textContent='Hello Wold....';
+newvalue.classList.add('Main');
+rootvalue.appendChild(newvalue);
---
+++ index.js
@@ -0,0 +1,6 @@
+const rootvalue=document.getElementById('root');
+const newvalue=document.createElement('div');
+newvalue.textContent='Hello Wold....';
+newvalue.classList.add('Main');
+rootvalue.appendChild(newvalue);
+
