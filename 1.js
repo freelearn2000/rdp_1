e9eb2af032129f87ac2c7390a223f5d03e956938
ReshmiAjay2022 <100823647+ReshmiAjay2022@users.noreply.github.com>
2023-03-11 13:56:01 +0530

    Revert "Reshmi Ajay"
    This reverts commit 6b601958422b7e1243a25b70807413a97210f691.

--- 1.js
+++
@@ -1,5 +0,0 @@
-const rootvalue=document.getElementById('root');
-const newvalue=document.createElement('div');
-newvalue.textContent='Hello Wold....';
-newvalue.classList.add('Main');
-rootvalue.appendChild(newvalue);
