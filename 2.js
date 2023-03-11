a6dc43c5af1e3b0528e84a3a8289c8e027e80023
ReshmiAjay2022 <100823647+ReshmiAjay2022@users.noreply.github.com>
2023-03-11 13:55:49 +0530

    Revert "update2.js"
    This reverts commit bb9e8e085fbde9d47b8e6f7f3e82882a5c420535.

--- 2.js
+++ 2.js
@@ -1,7 +1,7 @@
 const Component1  = ( props ) => {
     return (
         <div className='Component'>
-            <h3>Functional Component1</h3>
+            <h3 onClick={()=> props.xyz ('Hello')}>Functional Component1</h3>
             <h4>Name : {props.name}</h4>
             <h4>Age : {props.age}</h4>
             <span>
@@ -15,9 +15,9 @@ class Component2 extends React.Component {
     render( ){
         return (
             <div className='Component'>
-                <h3>Class Component2</h3>
-                <h4>Name : {this.props.name1}</h4>
-            <h4>Age : {this.props.age1}</h4>
+                <h3 onClick ={()=> this.props.xyz('Hai')} >Class Componnent2</h3>
+                <h4>Name : {this.props.name}</h4>
+            <h4>Age : {this.props.age}</h4>
                 <span>
                     <i>MODEL</i>
               </span>
@@ -29,14 +29,15 @@ class Component2 extends React.Component {
 class App extends React.Component {
     name = 'Steve';
     age=23;
-    name1 = 'Mike';
-    age1=32;
-        render ( ) {
+    somMethod (param) {
+        console.log('App Function called :',param);
+    }
+    render ( ) {
         return (
 <div>
     <h2>App Component</h2>
-    <Component1 name={this.name} age={this.age} ></Component1>
-    <Component2 name1={this.name1} age1={this.age1} ></Component2>
+    <Component1 name={this.name} age={this.age} xyz={this.somMethod()}></Component1>
+    <Component2 name={this.name} age={this.age} xyz={this.somMethod()}></Component2>
 
 </div>
         );
