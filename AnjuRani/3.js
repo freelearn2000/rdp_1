169c025fa8d5b9ca0cc67478e3ea6ab9a3241544
Biju Kumar <63250919+creativehub2000@users.noreply.github.com>
2023-03-16 18:12:39 +0530

    Reviewed

--- AnjuRani/3.js
+++ AnjuRani/3.js
@@ -1,63 +1,62 @@
-const menu = () =>{
-    return(
-        <div class="headerui"><a>Home</a>
-        <a class="item">About Us</a>
-        <a>Contact Us</a>
+const Menu = ( ) => {
+    return (
+        <div class="headerui">
+            <a>Home</a>
+            <a class="item">About Us</a>
+            <a>Contact Us</a>
         </div>
     );
 };
-const Header=()=>
-{
-    return(
+
+const Header = ( ) => {
+    return (
         <div className='App'>
-            <span>Home | About Us | Contact Us</span>
+            <Menu />
         </div>
     );
 };
-const Footer=()=>
+
+const Footer = ( ) =>
 {
-    return(
+    return (
         <div className='App'>
             <span>Copyright 2023 © Xenia Technologies All Rights reserved</span>
         </div>
     );
 };
-const Title=()=>
+
+const Title = ( ) =>
 {
-    return(
+    return (
         <div className='App'>
             <h1>React Training</h1>
         </div>
     );
 };
-const HomeComponent=()=>
-{
-   
-};
-const Component1  = ( props ) => {
+
+const HomeContent = ( props ) => {
     return (
         <div className='Component'>
+            <Title />
             <h3 className='Componenth3' onClick={()=> props.xyz ('Hello')}>Functional Component1</h3>
             <h4>Name : {props.name}</h4>
             <h4>Age : {props.age}</h4>
-            <span>
-                <i>MODEL</i>
-            </span>
         </div>
     );
 };
 
 class App extends React.Component {
-    render()
-    {
+    
+    render ( ) {
         return(
             <div className='App'>
                 <Header/>
-                <Title/>
+                <HomeContent name='Steve' age=23 />
                 <Footer/>
             </div>
         );
     }
 }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
