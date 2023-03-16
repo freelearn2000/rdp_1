1a930f5c5f24e3666c3a67f195436d9cf6d7c47d
Biju Kumar <63250919+creativehub2000@users.noreply.github.com>
2023-03-16 18:06:05 +0530

    Reviewed

--- AnjuRani/1.js
+++ AnjuRani/1.js
@@ -1,5 +1,5 @@
-const rootvalue=document.getElementById('root');
-const newvalue=document.createElement('div');
-newvalue.textContent='Hello Wold....';
+const rootvalue = document.getElementById('root');
+const newvalue = document.createElement('div');
+newvalue.textContent = 'Hello Wold....';
 newvalue.classList.add('Main');
 rootvalue.appendChild(newvalue);
