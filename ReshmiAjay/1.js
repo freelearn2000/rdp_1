0bcb576240f92eebc4433f4d8bf483a2cbcd4583
Biju Kumar <63250919+creativehub2000@users.noreply.github.com>
2023-03-16 18:19:33 +0530

    Reviewed

--- ReshmiAjay/1.js
+++ ReshmiAjay/1.js
@@ -1,5 +1,6 @@
-const rootvalue=document.getElementById('root');
-const newvalue=document.createElement('div');
+const newvalue = document.createElement('div');
 newvalue.textContent='Hello Wold....';
 newvalue.classList.add('Main');
+
+const rootvalue = document.getElementById('root');
 rootvalue.appendChild(newvalue);
