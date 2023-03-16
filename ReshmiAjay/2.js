6a677f682d5b201f318e05f2ea098926379b716a
Biju Kumar <63250919+creativehub2000@users.noreply.github.com>
2023-03-16 18:22:35 +0530

    Reviewed

--- ReshmiAjay/2.js
+++ ReshmiAjay/2.js
@@ -1,47 +1,45 @@
-const Component1  = ( props ) => {
+const Component1 = ( props ) => {
+    
     return (
         <div className='Component'>
             <h3>Functional Component1</h3>
             <h4>Name : {props.name}</h4>
             <h4>Age : {props.age}</h4>
-            <span>
-                <i>MODEL</i>
-            </span>
         </div>
     );
 };
 
 class Component2 extends React.Component {
-    render( ){
+    
+    render ( ) {
         return (
             <div className='Component'>
                 <h3>Class Component2</h3>
                 <h4>Name : {this.props.name1}</h4>
-            <h4>Age : {this.props.age1}</h4>
-                <span>
-                    <i>MODEL</i>
-              </span>
+                <h4>Age : {this.props.age1}</h4>
             </div>
         );
     }
 }
 
 class App extends React.Component {
-    name = 'Steve';
-    age=23;
-    name1 = 'Mike';
-    age1=32;
-        render ( ) {
+    
+    user = {name: 'Steve', age: 23};
+//     name = 'Steve';
+//     age=23;
+//     name1 = 'Mike';
+//     age1=32;
+    
+    render ( ) {
         return (
-<div>
-    <h2>App Component</h2>
-    <Component1 name={this.name} age={this.age} ></Component1>
-    <Component2 name1={this.name1} age1={this.age1} ></Component2>
-
-</div>
+            <div>
+                <h2>App Component</h2>
+                <Component1 name={this.user.name} age={this.user.age} ></Component1>
+//                 <Component2 name1={this.user.name1} age1={this.user.age1} ></Component2>
+            </div>
         );
     }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);n
+root.render(<App />);
