77943c9b2735b2293109c4ca9a7a38dc6d52d728
Biju Kumar <63250919+creativehub2000@users.noreply.github.com>
2023-03-16 18:03:00 +0530

    Reviewed

--- AnjuRani/2.js
+++ AnjuRani/2.js
@@ -4,9 +4,6 @@ const Component1  = ( props ) => {
             <h3 className='Componenth3' onClick={()=> props.xyz ('Hello')}>Functional Component1</h3>
             <h4>Name : {props.name}</h4>
             <h4>Age : {props.age}</h4>
-            <span>
-                <i>MODEL</i>
-            </span>
         </div>
     );
 };
@@ -17,31 +14,31 @@ class Component2 extends React.Component {
             <div className='Component'>
                 <h3 className='Componenth3' onClick ={()=> this.props.xyz('Hai')} >Class Componnent2</h3>
                 <h4>Name : {this.props.name}</h4>
-            <h4>Age : {this.props.age}</h4>
-                <span>
-                    <i>MODEL</i>
-              </span>
+                <h4>Age : {this.props.age}</h4>
             </div>
         );
     }
 }
 
 class App extends React.Component {
-    name = 'Steve';
-    age=23;
-    name1 = 'MiKe';
-    age1=33;
+    
+    user = {name: 'Steve', age: 23};
+//     name = 'Steve';
+//     age = 23;
+//     name1 = 'MiKe';
+//     age1 = 33;
+    
     somMethod (param) {
         console.log('App Function called :',param);
     }
+    
     render ( ) {
         return (
-<div>
-    <h2>App Component</h2>
-    <Component1 name={this.name} age={this.age} xyz={this.somMethod()}></Component1>
-    <Component2 name={this.name1} age={this.age1} xyz={this.somMethod()}></Component2>
-
-</div>
+            <div>
+                <h2>App Component</h2>
+                <Component1 name={this.user.name} age={this.user.age} xyz={this.somMethod()}></Component1>
+//                 <Component2 name={this.user.name} age={this.user.age} xyz={this.somMethod()}></Component2>
+            </div>
         );
     }
 }
