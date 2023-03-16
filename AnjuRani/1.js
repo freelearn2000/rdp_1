7477bd0729d4a4594b93eeb35e4838c9ef2346a4
Biju Kumar <63250919+creativehub2000@users.noreply.github.com>
2023-03-16 18:08:33 +0530

    Reviewd

--- AnjuRani/1.js
+++ AnjuRani/1.js
@@ -1,5 +1,10 @@
-const rootvalue = document.getElementById('root');
+// Create a new DIV Object
 const newvalue = document.createElement('div');
 newvalue.textContent = 'Hello Wold....';
 newvalue.classList.add('Main');
+
+// Get reference to root DIV
+const rootvalue = document.getElementById('root');
+
+// Attach the new DIV to root DIV
 rootvalue.appendChild(newvalue);
