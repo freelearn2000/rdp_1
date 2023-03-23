9b26b6ae6918ce6e018a589b22183b583661b7b6
“vvpranav04” <vvpranav04@gmail.com>
2023-03-23 16:18:24 +0530

    style changes

--- pranav/js/static_data.js
+++ pranav/js/static_data.js
@@ -2,8 +2,8 @@ const Component1=(props)=>{
   return (
       <div className='component'>
           <h3>Component1</h3>
-          <p><span>Name:{props.name}</span><br />
-          <span>Phone:{props.phone}</span></p>
+          <p><span>Name:{props.name}</span></p>
+          <p><span>Phone:{props.phone}</span></p>
       </div>
   );
 }
@@ -13,8 +13,8 @@ class Component2 extends React.Component{
       return (
           <div className='component'>
             <h3>Component2</h3>
-              <p><span>Name:{this.props.name}</span><br />
-              <span>phone:{this.props.phone}</span></p>
+              <p><span>Name:{this.props.name}</span></p>
+              <p><span>phone:{this.props.phone}</span></p>
           </div>
       );
   }
@@ -25,6 +25,8 @@ class App extends React.Component{
       return (
           <div className='appcls'>
               <Component1  name='Pranav' phone='7654234567' />
+              <Component1  name='Krishnapriya' phone='7654234567' />
+              <Component1  name='Gopika' phone='7654234567' />
               <Component2 name='Sreekanth' phone='1245678289' />
           </div>
       );
