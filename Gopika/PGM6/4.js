bb88384e360af7c4dec13bb8538e10a90483d404
sgopika <s.gopika84@gmail.com>
2023-03-23 15:02:53 +0530

    foldername change

--- Gopika/PGM6/4.js
+++ Gopika/PGM6/4.js
@@ -1,125 +1,41 @@
-const Header = () => {
-    return (
-        <header>
-           <div class="ui inverted menu">
-  <a class="active item">
-    Home
-  </a>
-  <a class="item">
-    About us
-  </a>
-  <a class="item">
-    Team
-  </a>
-   <a class="item">
-    Contact us
-  </a>
-</div>
-        </header>
-    );
-};
-
-const Main = () => {
-    return (
-        <>
-        <div class="ui link cards">
-        <div class="card">
-          <div class="image">
-            <img src="/images/matthew.png" />
-          </div>
-          <div class="content">
-            <div class="header">Matt Giampietro</div>
-            <div class="meta">
-              <a>Friends</a>
-            </div>
-            <div class="description">
-              Matthew is an interior designer living in New York.
-            </div>
-          </div>
-          <div class="extra content">
-            <span class="right floated">
-              Joined in 2013
-            </span>
-            <span>
-              <i class="user icon"></i>
-              75 Friends
-            </span>
-          </div>
-        </div>
-        <div class="card">
-          <div class="image">
-            <img src="images/molly.png" />
-          </div>
-          <div class="content">
-            <div class="header">Molly</div>
-            <div class="meta">
-              <span class="date">Coworker</span>
-            </div>
-            <div class="description">
-              Molly is a personal assistant living in Paris.
-            </div>
-          </div>
-          <div class="extra content">
-            <span class="right floated">
-              Joined in 2011
-            </span>
-            <span>
-              <i class="user icon"></i>
-              35 Friends
-            </span>
-          </div>
-        </div>
-        <div class="card">
-          <div class="image">
-            <img src="/images/elyse.png" />
-          </div>
-          <div class="content">
-            <div class="header">Elyse</div>
-            <div class="meta">
-              <a>Coworker</a>
-            </div>
-            <div class="description">
-              Elyse is a copywriter working in New York.
-            </div>
-          </div>
-          <div class="extra content">
-            <span class="right floated">
-              Joined in 2014
-            </span>
-            <span>
-              <i class="user icon"></i>
-              151 Friends
-            </span>
-          </div>
-        </div>
+const Component1=(props)=>{
+  return (
+      <div className='Main'>
+         <h3>Component1</h3>
+         <span>Name:{props.name}</span>
+         <span>Address:{props.address}</span>
       </div>
-      </>
-    );
+  );
+}
+class Component2 extends React.Component{
+  render(){
+      return (
+          <div className='Main1'>
+            <h3>Component2</h3>
+         <span>Name:{this.props.name}</span>
+         <span>Address:{this.props.address}</span>
+  
+          </div>
+      );
+  }
+
 }
 
-const Footer = () => {
-    return (
-      <>
-      <div class="ui inverted vertical footer segment form-page">
-  <div class="ui container">
-  copyright@CDIT. All Rights Reserved
-  </div>
-</div>
-</> 
-    );
-}
 
 class App extends React.Component{
-    render( ){
-        return (
-            <div className='Container'>
-                <Header />
-                <Main/>
-                <Footer/>
-            </div>
-        );
-    }
-}
+  render(){
+      return (
+          <div className='appcls'>
+              APP COMPONENT  !!!
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+              <Component1  name='Deepa' address='kowdiar' />
+              <Component2 name='Reshma' address='Statue' />
+  
+          </div>
+      );
+  }
+
+}
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
+
