b0ffbfe8504fe5d041ea558b1a98534984de9d52
“vvpranav04” <vvpranav04@gmail.com>
2023-03-23 16:30:52 +0530

    code format

--- pranav/js/main.js
+++ pranav/js/main.js
@@ -13,81 +13,81 @@ const Header = () => {
 };
 
 const Profile = () => {
-    return(
+    return (
 
         <div className="container">
             <div className="ui link cards">
                 <div className="card">
                     <div className="image">
-                    <img src="img/matthew.png" />
+                        <img src="img/matthew.png" />
                     </div>
                     <div className="content">
-                    <div className="header">Matt Giampietro</div>
-                    <div className="meta">
-                        <a>Friends</a>
-                    </div>
-                    <div className="description">
-                        Matthew is an interior designer living in New York.
-                    </div>
+                        <div className="header">Matt Giampietro</div>
+                        <div className="meta">
+                            <a>Friends</a>
+                        </div>
+                        <div className="description">
+                            Matthew is an interior designer living in New York.
+                        </div>
                     </div>
                     <div className="extra content">
-                    <span className="right floated">
-                        Joined in 2013
-                    </span>
-                    <span>
-                        <i className="user icon"></i>
-                        75 Friends
-                    </span>
+                        <span className="right floated">
+                            Joined in 2013
+                        </span>
+                        <span>
+                            <i className="user icon"></i>
+                            75 Friends
+                        </span>
                     </div>
                 </div>
                 <div className="card">
                     <div className="image">
-                    <img src="img/molly.png" />
+                        <img src="img/molly.png" />
                     </div>
                     <div className="content">
-                    <div className="header">Molly</div>
-                    <div className="meta">
-                        <span className="date">Coworker</span>
-                    </div>
-                    <div className="description">
-                        Molly is a personal assistant living in Paris.
-                    </div>
+                        <div className="header">Molly</div>
+                        <div className="meta">
+                            <span className="date">Coworker</span>
+                        </div>
+                        <div className="description">
+                            Molly is a personal assistant living in Paris.
+                        </div>
                     </div>
                     <div className="extra content">
-                    <span className="right floated">
-                        Joined in 2011
-                    </span>
-                    <span>
-                        <i className="user icon"></i>
-                        35 Friends
-                    </span>
+                        <span className="right floated">
+                            Joined in 2011
+                        </span>
+                        <span>
+                            <i className="user icon"></i>
+                            35 Friends
+                        </span>
                     </div>
                 </div>
                 <div className="card">
                     <div className="image">
-                    <img src="img/elyse.png" />
+                        <img src="img/elyse.png" />
                     </div>
                     <div className="content">
-                    <div className="header">Elyse</div>
-                    <div className="meta">
-                        <a>Coworker</a>
-                    </div>
-                    <div className="description">
-                        Elyse is a copywriter working in New York.
-                    </div>
+                        <div className="header">Elyse</div>
+                        <div className="meta">
+                            <a>Coworker</a>
+                        </div>
+                        <div className="description">
+                            Elyse is a copywriter working in New York.
+                        </div>
                     </div>
                     <div className="extra content">
-                    <span className="right floated">
-                        Joined in 2014
-                    </span>
-                    <span>
-                        <i className="user icon"></i>
-                        151 Friends
-                    </span>
+                        <span className="right floated">
+                            Joined in 2014
+                        </span>
+                        <span>
+                            <i className="user icon"></i>
+                            151 Friends
+                        </span>
                     </div>
                 </div>
             </div>
-        </div>    
+        </div>
     );
 }
 // for main sectionfdgdfdg
@@ -108,14 +108,14 @@ const Footer = () => {
     );
 }
 
-class App extends React.Component{
-    render( ){
+class App extends React.Component {
+    render() {
         return (
             <div className='app'>
                 <Header />
-                <Main/>
+                <Main />
                 <Profile />
-                <Footer/>
+                <Footer />
             </div>
         );
     }
--- pranav/js/static_data.js
+++ pranav/js/static_data.js
@@ -1,39 +1,38 @@
-const Component1=(props)=>{
+const Component1 = (props) => {
   return (
-      <div className='component'>
-          <h3>Component1</h3>
-          <p><span>Name:{props.name}</span></p>
-          <p><span>Phone:{props.phone}</span></p>
-      </div>
+    <div className='component'>
+      <h3>Component1</h3>
+      <p><span>Name:{props.name}</span></p>
+      <p><span>Phone:{props.phone}</span></p>
+    </div>
   );
 }
 
-class Component2 extends React.Component{
-  render(){
-      return (
-          <div className='component'>
-            <h3>Component2</h3>
-              <p><span>Name:{this.props.name}</span></p>
-              <p><span>phone:{this.props.phone}</span></p>
-          </div>
-      );
+class Component2 extends React.Component {
+  render() {
+    return (
+      <div className='component'>
+        <h3>Component2</h3>
+        <p><span>Name:{this.props.name}</span></p>
+        <p><span>phone:{this.props.phone}</span></p>
+      </div>
+    );
   }
 }
 
-class App extends React.Component{
-  render(){
-      return (
-          <div className='appcls'>
-              <Component1  name='Pranav' phone='7654234567' />
-              <Component1  name='Krishnapriya' phone='7654234567' />
-              <Component1  name='Gopika' phone='7654234567' />
-              <Component2 name='Sreekanth' phone='1245678289' />
-          </div>
-      );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='appcls'>
+        <Component1 name='Pranav' phone='7654234567' />
+        <Component1 name='Krishnapriya' phone='7654234567' />
+        <Component1 name='Gopika' phone='7654234567' />
+        <Component2 name='Sreekanth' phone='1245678289' />
+      </div>
+    );
   }
 }
 
-const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
-  
