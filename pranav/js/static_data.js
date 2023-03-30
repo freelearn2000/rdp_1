c49b5e921a20b3de427fbfa992aec816da7e9dad
“vvpranav04” <vvpranav04@gmail.com>
2023-03-30 15:56:23 +0530

    march 30

--- pranav/js/static_data.js
+++ pranav/js/static_data.js
@@ -20,11 +20,13 @@ class Component2 extends React.Component {
   }
 }
 
+
+
 class App extends React.Component {
   render() {
     return (
       <div className='appcls'>
-        <Component1 name='Pranav' phone='7654234567' />
+        <Component1 name='Pranav' phone='325523525' />
         <Component1 name='Krishnapriya' phone='7654234567' />
         <Component1 name='Gopika' phone='7654234567' />
         <Component2 name='Sreekanth' phone='1245678289' />
