c53e5c2eab3fb507c82c6e1942640bc145707219
sgopika <s.gopika84@gmail.com>
2023-03-30 16:47:33 +0530

    axios

--- Gopika/PGM9/5.js
+++ Gopika/PGM9/5.js
@@ -19,27 +19,53 @@ class News extends React.Component{
   }
   render(){
     this.getNewsreport();
+   
       return (
           <div className='Main1'>
-            <h3 onClick ={()=> this.props.xy('Hai inside Component2')}>News Component</h3>
-         <span>Name:{this.props.name}</span>
-         <span>Address:{this.props.address}</span>
+            <h3 >News Component</h3>
+         <span>Name:{this.getNewsreport.title}</span>
+         <span>Address:{this.getNewsreport.body}</span>
   
           </div>
       );
   }
 
 }
+//axios 
 class Weather extends React.Component{
+  state = { title: "", body: "" };
+
+  getModelCallback = (model) => {
+    console.log("Success : ");
+    console.log(model.data);
+    this.state.title = model.data.title;
+    this.state.body = model.data.body;
+    this.setState({ title: this.state.title, body: this.state.body });
+  };
+
+  getErrorCallback = (error) => {
+    console.log("Error : ");
+    console.log(error.message);
+  };
+
+
   render(){
       return (
-          <div className='Main1'>
-            <h3 onClick ={()=> this.props.xy('Hai inside Component2')}>Weather Component</h3>
-         <span>Name:{this.props.name}</span>
-         <span>Address:{this.props.address}</span>
+          <div className='Main'>
+            <h3>Weather Component</h3>
+         <p><b>Title:</b>{this.state.title}</p>
+         <p><b>Body:</b>{this.state.body}</p>
   
           </div>
       );
+  }
+//componentDidMount() only call after render method
+  componentDidMount() {
+       axios
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .then(this.getModelCallback)
+      .catch(this.getErrorCallback);
+    console.log("component Did Mount");
   }
 
 }
@@ -57,7 +83,7 @@ class App extends React.Component{
               APP COMPONENT  !!!
 
               <News  name={this.det.name} address={this.det.address} xy={this.Display} />
-              {/* <Weather  name={this.det.name} address={this.det.address} xy={this.Display} /> */}
+              <Weather  name={this.det.name} address={this.det.address} xy={this.Display} />
   
           </div>
       );
