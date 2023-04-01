7177f071e4ed30300ec26d32957aadd085147dca
deepthinh <deepthi.nh@gmail.com>
2023-04-01 14:02:57 +0530

    pg8 app component

---
+++ Deepthi/pg8/5.js
@@ -0,0 +1,45 @@
+const Component1=(props)=>{
+  return (
+      <div className='Main'>
+         <h3 onClick ={()=>props.xy('Hai inside Component1')}>Component1</h3>
+         <span>Name:{props.name}</span>
+         <span>Address:{props.address}</span>
+      </div>
+  );
+}
+class Component2 extends React.Component{
+  render(){
+      return (
+          <div className='Main1'>
+            <h3 onClick ={()=> this.props.xy('Hai inside Component2')}>Component2</h3>
+         <span>Name:{this.props.name}</span>
+         <span>Address:{this.props.address}</span>
+  
+          </div>
+      );
+  }
+
+}
+
+
+class App extends React.Component{
+  det = {name:'Deepa', address:'kowdiar'}
+  Display(params){
+    console.log('Display Function called :',params);
+  }
+  render(){
+   
+      return (
+          <div className='appcls'>
+              APP COMPONENT  !!!
+
+              <Component1  name={this.det.name} address={this.det.address} xy={this.Display} />
+              <Component2  name={this.det.name} address={this.det.address} xy={this.Display} />
+  
+          </div>
+      );
+  }
+
+}
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
